2f31c6147d5a6405acdf6e89b5608e5f29ac10b1
EgorGorbik <gorbik1966@mail.ru>
2020-03-02 00:58:35 +0300

    a

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 app.use(bodyParser.json());
 
-let refreshTokens = [];
+let accessTokens = [];
 
 app.get('/post', authenticateToken, (req, res) => {
     res.json(req.user)
@@ -17,13 +17,12 @@ app.post('/login', (req, res) => {
     const user = { name: username };
 
     const accessToken = generateAccessToken(user);
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-    refreshTokens.push(refreshToken)
-    res.json({ accessToken, refreshToken })
+    accessTokens.push(accessToken)
+    res.json({ accessToken })
 })
 
 app.delete('/logout', (req, res) => {
-    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    accessTokens = accessTokens.filter(token => token !== req.body.token);
     res.sendStatus(204)
 })
 
@@ -31,6 +30,7 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1];
     if(token == null) return res.sendStatus(401);
+    if(!accessTokens.find(e => e === token)) return res.sendStatus(403);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if(err) return res.sendStatus(403)
